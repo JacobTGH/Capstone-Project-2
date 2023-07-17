6e9e5a682d43b276c4e27885d04bfb587fe5137d
CamilaBarth <138497565+CamilaBarth@users.noreply.github.com>
2023-07-17 14:47:39 +0100

    Update recipe.py

--- Recipe_Manager/recipe.py
+++ Recipe_Manager/recipe.py
@@ -1,3 +1,4 @@
+
 class Recipe(): # Creating the recipe class
     
     def __init__(self, recipe_name, recipe_ingredients, recipe_instructions, recipe_cooking_time, recipe_dietary_info): # Constructor for new recipes        
@@ -47,3 +48,68 @@ class Recipe(): # Creating the recipe class
             i = int(i)
             print(f"{i + 1}. {self.instructions[i].capitalize()}")
             
+recipes = {} #Dictionary to store the recipes
+
+class recipe_management(): #Class for management of receipes
+    def add_recipe():
+        recipe_name = input("Enter recipe name: ")
+        recipe_ingredients = input("Enter ingredients (separated by commas): ").split(",")
+        recipe_instructions = input("Enter instructions: ")
+        recipe_cooking_time = input("Enter cooking time: ")
+        recipe_dietary = input("Enter dietary info: ")
+
+        recipe = {
+            "Name" : recipe_name,
+            "Ingredients" : recipe_ingredients,
+            "Instructions" : recipe_instructions,
+            "Cooking time" : recipe_cooking_time,
+            "Dietary" : recipe_dietary
+        }
+        recipes[recipe_name] = recipe
+        print ("Recipe add with success!")
+
+def update_recipe():
+ #options to choose what want to update and then show the existing information to update (?)
+
+ def view_recipe(recipe_name):
+    if recipe_name in recipes:
+        recipe = recipes[recipe_name]
+        print(f"Recipe: {recipe_name}")
+        print(f"Ingredients: {recipe['Ingredients']}")
+        print(f"Instructions: {recipe['Instructions']}")
+        print(f"Cooking time: {recipe['Cooking time']}")
+        print(f"Dietary: {recipe['Dietary']}")
+
+    def delete_recipe():
+        if recipe_name in recipes:
+            del recipes[recipe_name]
+            print("Recipe deleted!")
+        else:
+            print("Recipe could not be found. Try again.")
+
+    def menu():
+        while True:
+            print("--- Recipe system Management ---")
+            print("1. Add recipe")
+            print("2. Update recipe details")
+            print("3. View recipe")
+            print("4. Delete recipe")
+            print("5. Exit")
+
+            option = input("Enter your option (1-4): ")
+
+            if option == "1":
+                add_recipe()
+            elif option == "2":
+                recipe_name = input("Enter recipe name:")
+                update_recipe(recipe_name)
+            elif option == "3":
+                recipe_name = input("Enter recipe name: ")
+                view_recipe(recipe_name)
+            elif option == "4":
+                recipe_name = input("Enter recipe name: ")
+                delete_recipe(recipe_name)
+            elif option == "5":
+                break
+            else:
+                print("Invalid option. Choose option 1, 2, 3, 4 or 5.")
